14e355cc12a9d96586e8625e86c2a5e17a51bea5
Diana Shchetynina <Diana_Shchetynina@epam.com>
2017-05-23 00:43:25 +0300

    added helloworld file

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+/**
+ * Created by User on 4/11/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello WOrld!");
+    }
+}
